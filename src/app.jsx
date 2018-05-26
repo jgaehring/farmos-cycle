7fd52f67c8010b351cc29190d086c78faec6b2e2
Jamie Gaehring <gaehj457@gmail.com>
2018-05-26 09:31:48 -0400

    feat: add super simple input+btn

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,11 @@ import xs from 'xstream'
 
 export function App (sources) {
   const vtree$ = xs.of(
-    <div>My Awesome Cycle.js app</div>
+    <div>
+      <label for='name'>Name: </label>
+      <input className='name' type='text'/>
+      <button>Save</button>
+    </div>
   )
   const sinks = {
     DOM: vtree$
