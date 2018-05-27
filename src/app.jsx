c1b7c6530b132b07aea30aff0e9576fd1ce89dc6
Jamie Gaehring <gaehj457@gmail.com>
2018-05-27 11:34:39 -0400

    refactor: use MVI architecture

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,29 @@
 import xs from 'xstream'
 
-export function App (sources) {
-  const vtree$ = xs.of(
+function  intent(sources) {
+  return sources;
+};
+
+function model(actions) {
+  return actions;
+};
+
+function view(state) {
+  return xs.of(
     <div>
       <label for='name'>Name: </label>
       <input className='name' type='text'/>
       <button>Save</button>
     </div>
-  )
+  );
+};
+
+export function App (sources) {
+  const actions = intent(sources);
+  const state = model(actions);
+  const vtree$ = view(state);
   const sinks = {
     DOM: vtree$
   }
-  return sinks
+  return sinks;
 }
