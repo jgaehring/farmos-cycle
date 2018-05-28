e65cd5bd614d0efc1d76756f8c2b41f6cc807745
Jamie Gaehring <gaehj457@gmail.com>
2018-05-28 13:42:18 -0400

    feat: capture input streams

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,36 @@
 import xs from 'xstream'
 
-function  intent(sources) {
-  return sources;
+function intent(domSources) {
+  return domSources.select('.name')
+    .events('input')
+    .map(ev => ev.target.value);
 };
 
-function model(actions) {
-  return actions;
+function model(input$) {
+  const state$ = input$.startWith("");
+  return state$;
 };
 
-function view(state) {
-  return xs.of(
+function view(state$) {
+  return state$.map(val => (
     <div>
       <label for='name'>Name: </label>
-      <input className='name' type='text'/>
+      <input 
+        className='name' 
+        type='text' 
+        value={val}
+        placeholder="Enter a log name"
+      />
       <button>Save</button>
     </div>
-  );
+  ));
 };
 
 export function App (sources) {
-  const actions = intent(sources);
-  const state = model(actions);
-  const vtree$ = view(state);
+  const input$ = intent(sources.DOM);
+  const state$ = model(input$);
+  const vtree$ = view(state$);
+  
   const sinks = {
     DOM: vtree$
   }
