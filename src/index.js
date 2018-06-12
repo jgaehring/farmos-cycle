c58bc66d560b8282af5e87f997239a227a5cd786
Jamie Gaehring <gaehj457@gmail.com>
2018-06-12 11:27:27 -0400

    add cycle-idb driver

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import {run} from '@cycle/run'
 import {makeDOMDriver} from '@cycle/dom'
+import makeIdbDriver from 'cycle-idb'
 import {App} from './app'
 
 const main = App
 
 const drivers = {
   DOM: makeDOMDriver('#root'),
+  IDB: makeIdbDriver('farm-db', 1, upgrade => {
+      upgrade.createObjectStore('logs', {keyPath: 'id'})
+  }),
   log: msg$ => { msg$.addListener({next: msg => console.log(msg)})}
 }
 
